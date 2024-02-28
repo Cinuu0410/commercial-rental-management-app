8f8d14c793d406f48f3af45d6e4e870455c7d00e
Marcin <Cinuu0410@gmail.com>
2024-02-28 12:36:39 +0100

    Initial commit

---
+++ src/main/java/com/uwb/commercialrentalmanagementapp/CommercialRentalManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.uwb.commercialrentalmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialRentalManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercialRentalManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uwb/commercialrentalmanagementapp/CommercialRentalManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwb.commercialrentalmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialRentalManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
